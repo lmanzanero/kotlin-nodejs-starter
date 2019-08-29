b53e27bffac81cbdd45e796a3cc5a22682f2b3bf
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-08-28 21:45:58 -0600

    Added main activity to test with fake api

---
+++ app/src/main/java/com/example/luismanzanero/kotlinnodejsstarter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.luismanzanero.kotlinnodejsstarter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
