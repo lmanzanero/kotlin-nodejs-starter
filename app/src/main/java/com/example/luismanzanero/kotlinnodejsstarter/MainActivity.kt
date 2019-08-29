f1c9de6f9d0f95e94dcf58b26f56c0fdb2f67577
Luis Manzanero <manzanero.luis1995@gmail.com>
2019-08-28 22:10:29 -0600

    integrated fuel for network requests on click of button

--- app/src/main/java/com/example/luismanzanero/kotlinnodejsstarter/MainActivity.kt
+++ app/src/main/java/com/example/luismanzanero/kotlinnodejsstarter/MainActivity.kt
@@ -2,11 +2,25 @@ package com.example.luismanzanero.kotlinnodejsstarter
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.github.kittinunf.fuel.Fuel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainBtn.setOnClickListener {
+            getPosts()
+        }
+    }
+
+    fun getPosts(){
+        Fuel.get("https://jsonplaceholder.typicode.com/posts")
+            .responseString { request, response, result ->
+                println(result.get())
+            }
+
     }
 }
